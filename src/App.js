885d299774671c20979f411c1ce42947c2086602
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-04 16:18:43 +0530

    rank

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation'
 import Logo from './Components/Logo/Logo'
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
-
+import Rank from './Components/Rank/Rank'
 class App extends Component {
   render(){
     return(
       <div className="App">
 		<Navigation />
 		<Logo />
+    <Rank />
     <ImageLinkForm />
       </div>
     );
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Rank = () => {
+    return(
+        <div>
+        </div>
+    )
+}
+
+export default Rank;
