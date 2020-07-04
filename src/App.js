28e35af964af9234ef7fd13c5db4562fd93b7671
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-04 17:19:08 +0530

    button click

--- src/App.js
+++ src/App.js
@@ -30,6 +30,10 @@ class App extends Component {
   onInputChange = (event) => {
     console.log(event.target.value)
   }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
   render(){
 
     return(
@@ -40,7 +44,7 @@ class App extends Component {
           <Navigation />
           <Logo />
           <Rank />
-          <ImageLinkForm onInputChange={this.onInputChange} />
+          <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       </div>
     );
   }
