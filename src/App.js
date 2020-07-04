f141cae830797eac086f94f7bd3271e78649cc50
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-04 12:30:21 +0530

    template

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Navigation from './Components/Navigation/Navigation'
+
+function App() {
+  return (
+    <Navigation />
+  );
+}
+
+export default App;
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+        return (
+            <div>
+                <nav style={{display:'flex',justifyContent:'flex-end'}}>
+                <p className="f3 link dim black underline pa3 pointer">Sign Out</p>
+                </nav>
+            </div>
+        )
+}
+
+export default Navigation;
