b1d1e18b5d234a8480b2dede170d787dc4b498c2
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-04 16:16:46 +0530

    major update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation'
+import Logo from './Components/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 
-function App() {
-  return (
-    <Navigation />
-  );
+class App extends Component {
+  render(){
+    return(
+      <div className="App">
+		<Navigation />
+		<Logo />
+    <ImageLinkForm />
+      </div>
+    );
+  }
 }
-
 export default App;
