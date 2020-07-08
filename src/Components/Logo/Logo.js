18eafc445da1f2ce4a475a6ba4ab1cccf37708d3
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-08 16:11:01 +0530

    formatting

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -4,15 +4,15 @@ import './Logo.css';
 import brain from './brain.png'
 
 const Logo = () => {
-        return (
-            <div className="ma4 mt0">
-            <Tilt className="Tilt br2 shadow-2" options={{max : 50}}  style={{height: 150, width: 150}} >
+    return (
+        <div className="ma4 mt0">
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 50 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner pa3">
-									<img alt="brain" src={brain} />
+                    <img alt="brain" src={brain} />
                 </div>
             </Tilt>
-            </div>
-        )
+        </div>
+    )
 }
 
 export default Logo;
