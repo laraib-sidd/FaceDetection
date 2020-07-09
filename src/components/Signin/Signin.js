d8cf8ae376df9bb11c6917f6953dfe7504f7447c
laraib-sidd <laraib.sidd7@gmail.com>
2020-07-09 10:56:14 +0530

    file udpate

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react';
 
 class Signin extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      signInEmail: "",
-      signInPassword: "",
-    };
+      signInEmail: '',
+      signInPassword: ''
+    }
   }
 
   onEmailChange = (event) => {
-    this.setState({ signInEmail: event.target.value });
-  };
+    this.setState({signInEmail: event.target.value})
+  }
 
-  onPasswrodChange = (event) => {
-    this.setState({ signInPassword: event.target.value });
-  };
+  onPasswordChange = (event) => {
+    this.setState({signInPassword: event.target.value})
+  }
 
-  onSumbitSignin = () => {
-    fetch("https://warm-cove-10540.herokuapp.com/signin", {
-      method: "post",
-      headers: { "Content-Type": "application/json" },
+  onSubmitSignIn = () => {
+    fetch('https://warm-cove-10540.herokuapp.com/signin', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
         email: this.state.signInEmail,
-        passsword: this.state.signInPassword,
-      }),
+        password: this.state.signInPassword
+      })
     })
-      .then((res) => res.json())
-      .then((user) => {
+      .then(response => response.json())
+      .then(user => {
         if (user.id) {
-          this.props.loadUser();
-          this.props.onRouteChange("home");
+          this.props.loadUser(user)
+          this.props.onRouteChange('home');
         }
-      });
-  };
+      })
+  }
 
   render() {
     const { onRouteChange } = this.props;
@@ -42,48 +42,38 @@ class Signin extends React.Component {
         <main className="pa4 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0 center">Sign In</legend>
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">
-                  Email
-                </label>
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
-                  onChange={this.onEmailChange}
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
                   id="email-address"
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">
-                  Password
-                </label>
+                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
-                  onChange={this.onPasswrodChange}
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
                   id="password"
+                  onChange={this.onPasswordChange}
                 />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={this.onSumbitSignin}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in"
               />
             </div>
             <div className="lh-copy mt3">
-              <p
-                onClick={() => onRouteChange("register")}
-                href="#0"
-                className="pointer f6 link dim black db"
-              >
-                Register
-              </p>
+              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
         </main>
